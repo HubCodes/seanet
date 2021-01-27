9df616d38c44dd5a4e4f66c87d6437da7f660de6
Taemin Cha <krlrhkstk12@gmail.com>
2021-01-27 19:13:57 +0900

    New: basic task interface

--- task.h
+++ task.h
@@ -1,7 +1,18 @@
 #ifndef SEANET_TASK_H
 #define SEANET_TASK_H
 
-struct task;
-typedef struct task task_t;
+#include <ucontext.h>
+
+typedef enum task_state {
+    RUN,
+    SUSPEND,
+    END,
+} task_state_t;
+
+typedef struct task {
+    struct ucontext_t ctxt;
+    task_state_t state;
+    void* executable;
+} task_t;
 
 #endif //SEANET_TASK_H
